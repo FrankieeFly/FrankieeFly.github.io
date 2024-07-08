2bf622b86f41e595b87f89253d67d06b49eb4530
FrankieFly <Frankie_Fly@163.com>
2024-07-08 15:44:09 +0800

    Site updated: 2024-07-08 15:44:05

---
+++ js/f12.js
@@ -0,0 +1,15 @@
+document.onkeydown = function (e) {
+  if (123 == e.keyCode || (e.ctrlKey && e.shiftKey && (74 === e.keyCode || 73 === e.keyCode || 67 === e.keyCode)) || (e.ctrlKey && 85 === e.keyCode)) return new Vue({
+    data: function () {
+        this.$notify({
+            title: "你已被发现😜",
+            message: "小伙子，扒源记住要遵循GPL协议！",
+            position: 'top-left',
+            offset: 50,
+            showClose: true,
+            type: "warning",
+            duration: 5000
+        });
+    }
+}), event.keyCode = 0, event.returnValue = !1, !1
+};
